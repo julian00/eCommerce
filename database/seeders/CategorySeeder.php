2788c337314f9910a3716ce43d2bebdbb2204dbe
julian00 <ajarizaga@gmail.com>
2021-05-24 20:57:43 -0300

    seeders subcategories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -13,6 +15,37 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+            [
+                'name' => 'Celulares y tablets',
+                'slug' => Str::slug('Celulares y tablets'),
+                'icon' => '<i class="fas fa-mobile-alt"></i>'
+            ],
+            [
+                'name' => 'TV, audio y video',
+                'slug' => Str::slug('TV, audio y videos'),
+                'icon' => '<i class="fas fa-tv"></i>'
+            ],
+            [
+                'name' => 'Consolas y videojuegos',
+                'slug' => Str::slug('Consolas y videojuegos'),
+                'icon' => '<i class="fas fa-gamepad"></i>'
+            ],
+            [
+                'name' => 'Computacion',
+                'slug' => Str::slug('Computacion'),
+                'icon' => '<i class="fas fa-laptop"></i>'
+            ],
+            [
+                'name' => 'Moda',
+                'slug' => Str::slug('Moda'),
+                'icon' => '<i class="fas fa-tshirt"></i>'
+            ]
+        ];
+
+        foreach ($categories as $category) 
+        {
+            Category::factory(1) -> create($category);
+        }
     }
 }
--- database/seeders/SubCategorySeeder.php
+++ database/seeders/SubCategorySeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\SubCategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class SubCategorySeeder extends Seeder
 {
@@ -13,6 +15,126 @@ class SubCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $subcategories = [
+            //celulares y tablets
+            [
+                'category_id' => '1',
+                'name' => 'Celulares y smartphones',
+                'slug' => Str::slug('Celulares y smartphones'),
+                'color' => true
+            ],
+
+            [
+                'category_id' => '1',
+                'name' => 'Accesorios para celulares',
+                'slug' => Str::slug('Accesorios para celulares')
+            ],
+
+            [
+                'category_id' => '1',
+                'name' => 'Smartwatches',
+                'slug' => Str::slug('Smartwatches'),
+                'color' => true
+            ],
+
+            //Tv, audio y video
+            [
+                'category_id' => '2',
+                'name' => 'TV y audio',
+                'slug' => Str::slug('TV y audio')
+            ],
+
+            [
+                'category_id' => '2',
+                'name' => 'Audio',
+                'slug' => Str::slug('Audio')
+            ],
+
+            [
+                'category_id' => '2',
+                'name' => 'Audio para autos',
+                'slug' => Str::slug('Audio para autos')
+            ],
+
+            //Consola y videojuegos
+            [
+                'category_id' => '3',
+                'name' => 'Xbox',
+                'slug' => Str::slug('Xbox')
+            ],
+
+            [
+                'category_id' => '3',
+                'name' => 'Play Statios',
+                'slug' => Str::slug('Play Station')
+            ],
+
+            [
+                'category_id' => '3',
+                'name' => 'Videojuegos para PC',
+                'slug' => Str::slug('Videojuegos para PC')
+            ],
+
+            [
+                'category_id' => '3',
+                'name' => 'Nintendo',
+                'slug' => Str::slug('Nintendo')
+            ],
+
+            //Computacion
+            [
+                'category_id' => '4',
+                'name' => 'Portatiles',
+                'slug' => Str::slug('Portatiles')
+            ],
+
+            [
+                'category_id' => '4',
+                'name' => 'PC de escritorio',
+                'slug' => Str::slug('PC de escritorio')
+            ],
+
+            [
+                'category_id' => '4',
+                'name' => 'Almacenamiento',
+                'slug' => Str::slug('Almacenamiento')
+            ],
+
+            [
+                'category_id' => '4',
+                'name' => 'Accesorios',
+                'slug' => Str::slug('Accesorios')
+            ],
+
+            //Moda
+            [
+                'category_id' => '5',
+                'name' => 'Mujer',
+                'slug' => Str::slug('Mujer')
+            ],
+
+            [
+                'category_id' => '5',
+                'name' => 'Hombre',
+                'slug' => Str::slug('Hombre')
+            ],
+
+            [
+                'category_id' => '5',
+                'name' => 'Lentes',
+                'slug' => Str::slug('Lentes')
+            ],
+
+            [
+                'category_id' => '5',
+                'name' => 'Relojes',
+                'slug' => Str::slug('Relojes')
+            ],
+        ];
+
+        foreach ($subcategories as $subcategory)
+        {
+            SubCategory::factory(1)->create($subcategory);
+        }
     }
 }
