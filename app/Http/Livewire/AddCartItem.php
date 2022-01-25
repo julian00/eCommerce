ee55f8964fc1836ce726e34d7783e429579536ed
julian00 <ajarizaga@gmail.com>
2022-01-24 23:34:06 -0300

    agrege el archivo helpers funciones para limitar el stock en el carrito

--- app/Http/Livewire/AddCartItem.php
+++ app/Http/Livewire/AddCartItem.php
@@ -10,7 +10,10 @@ class AddCartItem extends Component
 {
     public $product, $quantity;
     public $qty = 1;
-    public $options = [];
+    public $options = [
+        'color_id' => null,
+        'size_id' => null
+    ];
 
     public function mount()
     {
